f4b9a1253944197ab68d52acafe337b40335175c
xStarH4ndx <brunotoro1799@gmail.com>
2025-04-27 22:02:25 -0400

    backup-sistemaBdd

--- bd/init.sql
+++
@@ -1,9 +0,0 @@
--- Crear tabla de ejemplo
-CREATE TABLE IF NOT EXISTS usuarios (
-                                        id SERIAL PRIMARY KEY,
-                                        nombre VARCHAR(100),
-    email VARCHAR(100) UNIQUE
-    );
-
--- Insertar datos de prueba
-INSERT INTO usuarios (nombre, email) VALUES ('Kamila', 'kamila@ejemplo.com');
